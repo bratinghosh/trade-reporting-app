b3b645f8a2b9da65ee491d2b347aca930dcba1b9
bratinghosh <ghosh.bratin13@gmail.com>
2023-02-21 15:46:47 +0800

    added mock data for broker table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- Mock Data for BROKER Table
+INSERT INTO BROKER VALUES ('Broker-A', 'Broker A');
+INSERT INTO BROKER VALUES ('Broker-B', 'Broker B');
+INSERT INTO BROKER VALUES ('Broker-C', 'Broker C');
+
+-- Mock Data for PRODUCT Table
+
+-- Mock Data for TRADE TABLE
