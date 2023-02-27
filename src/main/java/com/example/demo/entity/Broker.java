c651c1653877cbcd62b5b66ac70685fe099ed6cf
bratinghosh <ghosh.bratin13@gmail.com>
2023-02-27 14:50:26 +0800

    added constructors to entities

--- src/main/java/com/example/demo/entity/Broker.java
+++ src/main/java/com/example/demo/entity/Broker.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +12,8 @@ import javax.persistence.Table;
 @Entity
 @Table
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Broker {
     @Id
     @Column
--- src/main/java/com/example/demo/entity/Trade.java
+++ src/main/java/com/example/demo/entity/Trade.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -8,6 +10,8 @@ import java.util.Date;
 @Entity
 @Table
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Trade {
     @Id
     @Column
